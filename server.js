fb2be1178de7ab1e0d2b8fe93e1d0599d43662b1
RakeshLakhara <74376057+RakeshLakhara@users.noreply.github.com>
2021-01-22 02:28:59 +0530

    installed all dependencies

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/a", (_req, res) => {
+        res.json({ message: "Welcome to Rtg's application." });
+    });
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
